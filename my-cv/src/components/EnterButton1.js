e4d1f52635b2f4e405e6ea42ee05d06359ee2a16
Nadia Djellali <djellali.nadia@gmail.com>
2018-07-14 22:27:54 +0200

    first test for enter button on home page

---
+++ my-cv/src/components/EnterButton1.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { link } from '@reach/router'
+
+import './EnterButton1.css'
+
+class EnterButton1 extends React.Component {
+  render(){ 
+		return (
+			<div>
+			<h1 id="glow">Voir mon C.V</h1>
+		</div>
+  )
+ }
+}
+
+export default EnterButton1
