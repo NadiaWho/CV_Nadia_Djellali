990e0b39b46e6ba9fd65317e6fa532c12d7a251a
Nadia Djellali <djellali.nadia@gmail.com>
2018-07-15 12:50:10 +0200

    this choice button for home page

---
+++ my-cv/src/components/EnterButton4.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './EnterButton4.css'
+
+class EnterButton4 extends React.Component {
+  render () {
+    return (
+      <div class="sign">
+        <div class="neon-blue">V<span class="sizzle">oir</span> mo<span class="sizzle">n</span></div>
+        <div class="neon-blue"><span class="neon-white">C</span><span class="point">.</span><span class="neon-white" id="blink">V</span></div>
+      </div>
+    )
+  }
+}
+
+export default EnterButton4
