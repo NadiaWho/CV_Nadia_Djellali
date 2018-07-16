d640182aee8d329feb17b0c46a4d9a5d767548c2
Nadia Djellali <djellali.nadia@gmail.com>
2018-07-16 15:32:44 +0200

    new folder, 2nd page

---
+++ my-cv/src/components/Curriculum.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Curriculum.css'
+
+import SkillsLogos from './SkillsLogos';
+
+
+class Curriculum extends React.Component {
+    render () {
+        return (
+            <div id='curriculum'>
+            <SkillsLogos />
+            </div>
+        )
+    }
+}
+
+export default Curriculum
--- my-cv/src/index.js
+++ my-cv/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router } from '@reach/router'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <Router>
+      <App path='/*' />
+    </Router>, document.getElementById('root'))
 registerServiceWorker()
+  
