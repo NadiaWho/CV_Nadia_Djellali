78641d8fd15e7ff5f9d6bc708d5334e96ad065ea
Nadia Djellali <djellali.nadia@gmail.com>
2018-07-16 19:10:49 +0200

    change css of skills

--- my-cv/src/components/SkillsLogos.js
+++ my-cv/src/components/SkillsLogos.js
@@ -15,10 +15,14 @@ class SkillsLogos extends React.Component {
                 <ul>
                     <li><i className="fab fa-react"></i></li>
                     <li><i className="fab fa-node-js"></i></li>
+                    <li><i className="devicon-mysql-plain colored"></i></li>    
                 </ul>
                 <ul>
-                   <li><i className="fas fa-code-branch"></i></li>
-
+                   <li><i className="devicon-bootstrap-plain colored"></i></li>
+                   <li><i className="devicon-git-plain colored"></i></li>
+                   <li><i className="devicon-travis-plain colored"></i></li>
+                   
+		
                 </ul>
             </div>
             </Container>
